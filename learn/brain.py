b81820a90f362bed9cf93877cd5a49a0fa7e25c8
Lockps <Rulic1137@gmail.com>
2024-01-01 04:01:59 +0700

    the 1st version

--- learn/brain.py
+++ learn/brain.py
@@ -4,31 +4,32 @@ import random
 
 
 class Brain:
-    def __init__(self, inputs):
+    def __init__(self, inputs, clone=False):
         self.connections = []
         self.nodes = []
         self.inputs = inputs
         self.net = []
-        self.layer = 2
+        self.layers = 2
 
-        # Create input node
-        for i in range(0, self.inputs):
-            self.nodes.append(node.Node(i))
-            self.nodes[i].layer = 0
-        # Create bias node
-        self.nodes.append(node.Node(3))
-        self.nodes[3].layer = 0
-        # Create output node
-        self.nodes.append(node.Node(4))
-        self.nodes[4].layer = 1
+        if not clone:
+            # Create input node
+            for i in range(0, self.inputs):
+                self.nodes.append(node.Node(i))
+                self.nodes[i].layer = 0
+            # Create bias node
+            self.nodes.append(node.Node(3))
+            self.nodes[3].layer = 0
+            # Create output node
+            self.nodes.append(node.Node(4))
+            self.nodes[4].layer = 1
 
-        # Create Connections
-        for i in range(0, 4):
-            self.connections.append(
-                connection.Connection(
-                    self.nodes[i], self.nodes[4], random.uniform(-1, 1)
+            # Create Connections
+            for i in range(0, 4):
+                self.connections.append(
+                    connection.Connection(
+                        self.nodes[i], self.nodes[4], random.uniform(-1, 1)
+                    )
                 )
-            )
 
     def connect_nodes(self):
         for i in range(0, len(self.nodes)):
@@ -40,7 +41,7 @@ class Brain:
     def generate_net(self):
         self.connect_nodes()
         self.net = []
-        for j in range(0, self.layer):
+        for j in range(0, self.layers):
             for i in range(0, len(self.nodes)):
                 self.net.append(self.nodes[i])
 
@@ -58,3 +59,28 @@ class Brain:
             self.nodes[i].input_value = 0
 
         return output_value
+
+    def clone(self):
+        clone = Brain(self.inputs, True)
+
+        for n in self.nodes:
+            clone.nodes.append(n.clone())
+
+        for c in self.connections:
+            clone.connections.append(
+                c.clone(clone.getNode(c.from_node.id), clone.getNode(c.to_node.id))
+            )
+
+            clone.layers = self.layers
+            clone.connect_nodes()
+            return clone
+
+    def getNode(self, id):
+        for n in self.nodes:
+            if n.id == id:
+                return n
+
+    def mutate(self):
+        if random.uniform(0, 1) < 0.8:
+            for i in range(0, len(self.connections)):
+                self.connections[i].mutate_weight()
